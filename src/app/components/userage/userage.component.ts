c28e9cce82bba259c11a44838e2e60d4cf9eeb88
Gerliedao-ayan <gerliedaoayan02@gmail.com>
2024-09-20 19:25:42 +0900

    usergreeting

--- src/app/components/userage/userage.component.ts
+++ src/app/components/userage/userage.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './userage.component.css'
 })
 export class UserageComponent {
-  birthYer: number = 0;
+  birtYer: number = 0;
   // @ts-ignore
   age: number = null;
 
   calculateAge() {
     const currentYear = new Date().getFullYear();
-    this.age = currentYear - this.birthYer;
+    this.age = currentYear - this.birtYer;
   }
 }
